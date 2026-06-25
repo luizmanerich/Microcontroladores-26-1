2f15e6d28f750ab04aeae1feaaa567f46ab4d738
luizmanerich <luiz@uniavan.edu.br>
2026-06-25 20:22:59 -0300

    Add header file for Biblioteca Automatizada

--- BibliotecaAutomatizada.h
+++ BibliotecaAutomatizada.h
@@ -1,1 +1,20 @@
-a
+#ifndef BIBLIOTECAAUTOMATIZADA_H
+#define BIBLIOTECAAUTOMATIZADA_H
+
+#include <Arduino.h>
+
+void iniciarSistema(
+    int btProximo,
+    int btAnterior,
+    int btEmprestimo,
+    int btDevolucao,
+    int led1,
+    int led2,
+    int led3,
+    int led4,
+    int led5
+);
+
+void executarSistema();
+
+#endif
